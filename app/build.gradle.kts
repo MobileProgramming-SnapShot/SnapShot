e6ae420c372fcec951056b2f3cdc27578b2cef8c
Wonjun Jung <jwj0620@gachon.ac.kr>
2025-05-29 00:45:54 +0900

    profile_changed.5/29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,4 +111,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.11.0")
+
 }
